16b3ed6f9fa23725ea469890344b8f9e48dd66a3
The Ink Serpent <82593829+the-ink-serpent@users.noreply.github.com>
2025-09-09 20:20:29 +0530

    script

--- pywaladjustment.sh
+++ pywaladjustment.sh
@@ -1,10 +1,15 @@
+echo "removing old symlinked pywal files"
+
 rm ~/.config/hypr/hyprland/pywal-color.conf
 rm ~/.config/kitty/pywal-color.conf
+rm ~/.config/starship.toml
 rm ~/.config/quickshell/SerpentRice/PywalColor.qml
 
 echo "symlinking pywal files"
+
 ln ~/.cache/wal/colors-hyprland.conf ~/.config/hypr/hyprland/pywal-color.conf
 ln ~/.cache/wal/colors-kitty.conf ~/.config/kitty/pywal-color.conf
+ln ~/.cache/wal/starship-pywal-color.toml ~/.config/starship.toml
 ln ~/.cache/wal/pywal-color.qml ~/.config/quickshell/SerpentRice/PywalColor.qml
 
 echo "symlinking finished"
